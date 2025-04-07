7449ba7efd3b6b6a04ba69ea1a1c03d084f1fdb6
Abdoulaye-SAKHO <abdoulaye.sakho@artefact.com>
2025-04-07 16:28:15 +0200

    Update docstrings of DRF Mixin

--- mgs_grf/forest_for_categorical.py
+++ mgs_grf/forest_for_categorical.py
@@ -18,6 +18,10 @@ def iterative_random_choice(probas):
 
 
 class DrfFitPredictMixin:
+    """
+    Mixin for the Genralized Random Forest Procedure.
+    The predict draw a sample based on the frequency of training samples ending in the same leaf as the new sample.
+    """
     def fit(self, X, y, sample_weight=None):
         super().fit(X=X, y=y, sample_weight=sample_weight)
         self.train_y = y
@@ -32,27 +36,18 @@ class DrfFitPredictMixin:
         leaves_match = np.array(
             [leafs_by_sample[i] == self.train_samples_leaves for i in range(len(X))]
         )
-        #### NEEEW ####
-        # start_old = time.time()
-        ### taille n_samples x n_train x n_trees
-        # leafs_by_sample_repeat = np.tile(leafs_by_sample.T, (len(self.train_samples_leaves), 1, 1)).T
-        # print("repeat : ", time.time()-start_old)
-        # leaves_match = leafs_by_sample_repeat == self.train_samples_leaves.T
-        ### taille n_samples x n_trees x n_train
-        # print("Weights leaves match NEW : ", time.time()-start_old)
-        # n_by_tree = leaves_match.sum(axis=2)
-        # w = (leaves_match / n_by_tree[:,:, np.newaxis]).mean(axis=1) # taille n_samples x n_train
-        #### END NEEW #####
-
         n_by_tree = leaves_match.sum(axis=1)[:, np.newaxis, :]
         # leaves_match = leaves_match.astype(np.float16)
         # leaves_match /= n_by_tree
         # w = leaves_match.mean(axis=2) # taille n_samples x n_train
-        # print('b')
         w = (leaves_match / n_by_tree).mean(axis=2)  # taille n_samples x n_train
         return w
 
     def predict(self, X, batch_size=None):
+        """
+        Preditc procedure of GRF.
+        batch_size : int
+        """
         if batch_size is None:
             weights = self.get_weights(X)
         else:
